b4d0525bff3e824ea188d5069756799728b4c775
Preet <preetakamonk@gmail.com>
2025-12-13 15:21:13 +0530

    Change API key in fetch_data.py
    Updated API key for data fetching.

--- fetch_data.py
+++ fetch_data.py
@@ -4,7 +4,7 @@ import json
 import time
 import numpy as np
 
-API_KEY = "633379bdd5c4c3eb26919d8570866801e1c07f399197ba8c5311446b8ea77a49"
+API_KEY = "1eac22f8ec8e6da731a49adeae1148f14d6ceca13db5a9ffba65618f97406f4e"
 API_BASE_URL = "https://api.football-data-api.com/lastx"
 API_LEAGUE_URL = "https://api.football-data-api.com/league-season"
 
